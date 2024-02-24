6126ee06d8cacd55ec746dad177930f8f78d1859
LuisD <luis.domiciano@hotmail.com>
2024-02-24 20:51:57 -0300

    feat: find by id

--- src/main/java/com/luisd/japi/resources/UserResource.java
+++ src/main/java/com/luisd/japi/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.luisd.japi.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,13 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luisd.japi.domain.UserDomain;
+import com.luisd.japi.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
   
+  private @Autowired UserService userService;
+
   @GetMapping(value = "/{id}")
   public ResponseEntity<UserDomain> findById(@PathVariable Integer id) {
-    return ResponseEntity.ok().body(new UserDomain(1, "james", "james@mail.com", "aij2ijijsa"));
+    return ResponseEntity.ok().body(userService.findById(id));
   }
 }
--- src/main/java/com/luisd/japi/service/impl/UserServiceImpl.java
+++ src/main/java/com/luisd/japi/service/impl/UserServiceImpl.java
@@ -1,16 +1,22 @@
 package com.luisd.japi.service.impl;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luisd.japi.domain.UserDomain;
+import com.luisd.japi.repostories.UserRepository;
 import com.luisd.japi.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
 
+  private @Autowired UserRepository userRepository;
   @Override
   public UserDomain findById(Integer id) {
-    return null;
+    Optional<UserDomain> obj = userRepository.findById(id); 
+    return obj.orElse(null);
   }
   
 }
