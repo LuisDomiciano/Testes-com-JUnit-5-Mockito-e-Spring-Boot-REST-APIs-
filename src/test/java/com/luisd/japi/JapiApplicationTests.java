a20ed856991322b367cc03f49624a2c679148f37
LuisD <luis.domiciano@hotmail.com>
2024-02-24 19:33:03 -0300

    feat: initial commit

---
+++ src/test/java/com/luisd/japi/JapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisd.japi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
