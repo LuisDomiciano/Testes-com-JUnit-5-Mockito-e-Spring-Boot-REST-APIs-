f28fb83a8c9ccbe588dfad8448449fca30bcb4a6
LuisD <luis.domiciano@hotmail.com>
2024-02-26 23:46:46 -0300

    test: when find by id then return an user instance

---
+++ src/test/java/com/luisd/japi/service/impl/UserServiceImplTest.java
@@ -0,0 +1,77 @@
+package com.luisd.japi.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.luisd.japi.domain.UserDomain;
+import com.luisd.japi.domain.dto.UserDTO;
+import com.luisd.japi.repostories.UserRepository;
+
+@SpringBootTest
+public class UserServiceImplTest {
+
+  private static final Integer ID = 1;
+  private static final String NAME = "Zoloide";
+  private static final String EMAIL = "zo@mail.com";
+  private static final String PASSWORD = "aiP2j7Is";
+  
+  private @InjectMocks UserServiceImpl userServiceImpl;
+  private @Mock UserRepository userRepository;
+  private @Mock ModelMapper modelMapper;
+  private UserDomain user;
+  private UserDTO userDTO;
+  private Optional<UserDomain> optionalUser;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    startUser();
+  }
+
+  @Test
+  void testCreate() {
+
+  }
+
+  @Test
+  void testDelete() {
+
+  }
+
+  @Test
+  void testFindAll() {
+
+  }
+
+  @Test
+  void whenFindByIdThenReturnAnUserInstance() {
+    when(userRepository.findById(anyInt())).thenReturn(optionalUser);
+    UserDomain response = userServiceImpl.findById(ID);
+    assertNotNull(response);
+    assertEquals(UserDomain.class, response.getClass());
+    assertEquals(ID, response.getId());
+  }
+
+  @Test
+  void testUpdate() {
+
+  }
+
+  private void startUser() {
+    user = new UserDomain(ID, NAME, EMAIL,  PASSWORD);
+    userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    optionalUser = Optional.of(new UserDomain(ID, NAME, EMAIL, PASSWORD));
+}
+}
